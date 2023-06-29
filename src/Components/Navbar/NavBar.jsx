24576b9d361c093481990bb01cfe8b9b2c651555
Oyewale <oyewalefavour424@gmail.com>
2023-06-29 15:55:40 +0100

    wallet connect

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -7,39 +7,81 @@ import {
   BulbOutlined,
   FundOutlined,
   MenuOutlined,
-  SettingOutlined
+  SettingOutlined,
+  BorderInnerOutlined,
 } from "@ant-design/icons";
 import icon from "../../assets/logopng.png";
 
 const NavBar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreeensize] = useState(null);
+
+  useEffect(() => {
+    const resizeHandler = () => setScreeensize(window.innerWidth);
+
+    window.addEventListener("resize", resizeHandler);
+
+    resizeHandler();
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div>
       <div className="nav-container">
         <div className="logo-container">
-          <Avatar src={icon} size="large" />
-          <Typography.Title level={2} className="logo">
-            <Link to="/home">CryptoPark</Link>
-          </Typography.Title>
+          <div className="mobile-logo"> 
+            <Avatar src={icon} size="large" />
+            <Typography.Title level={4} className="logo">
+              <Link to="/home">$THE-Token</Link>
+            </Typography.Title>
+            {/* for button */}
+            <div className="pl-5">
+              <button className="px-4 py-2 text-white cursor-pointer font-raj bg-[#10bbc7] hover:bg-[#03e9f4] rounded-md font-semibold text-[13px] border-[#03e9f4] shadow-[#03e9f4] ">Buy $THE</button>
+            </div>
+            <div></div>
+          </div>
+          <Button
+            className="menu-control-container"
+            onClick={() => setActiveMenu(!activeMenu)}
+          >
+            <MenuOutlined />
+          </Button>
         </div>
-        <div>
-            <Menu theme="dark">
+
+        {activeMenu && (
+          <div>
+            <div>
+              <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined />}>
-                    <Link to="/home">Home</Link>
+                  <Link to="/home">$THE Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BorderInnerOutlined />}>
+                  <Link to="/home">$THE Presales</Link>
                 </Menu.Item>
                 <Menu.Item icon={<FundOutlined />}>
-                    <Link to="/cryptocurrency">Cryptocurrency</Link>
+                  <Link to="/cryptocurrency">$THE Lock</Link>
                 </Menu.Item>
                 <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to="/exchanges">Exchanges</Link>
+                  <Link to="/exchanges">$THE Liquidity Locks</Link>
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined />}>
-                    <Link to="/news">News</Link>
+                  <Link to="/news">News</Link>
                 </Menu.Item>
                 <Menu.Item icon={<SettingOutlined />}>
-                    <Link to="/setting">Settings</Link>
+                  <Link to="/setting">KYC & Marketing</Link>
                 </Menu.Item>
-            </Menu>
-        </div>
+              </Menu>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/Components/WalletConnect/WalletConnect.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Core } from "@walletconnect/core";
+import { Web3Wallet } from "@walletconnect/web3wallet";
+
+const core = new Core({
+  projectId: process.env.PROJECT_ID,
+});
+
+// const web3wallet = await Web3Wallet.init({
+//   core, // <- pass the shared `core` instance
+//   metadata: {
+//     name: "Demo app",
+//     description: "Demo Client as Wallet/Peer",
+//     url: "www.walletconnect.com",
+//     icons: [],
+//   },
+// });
+
+const WalletConnect = () => {
+  const [signClient, setSignClient] = useState();
+
+  async function createClient() {
+    try {
+      const web3wallet = await Web3Wallet.init({
+        core, // <- pass the shared `core` instance
+        metadata: {
+          name: "Demo app",
+          description: "Demo Client as Wallet/Peer",
+          url: "www.walletconnect.com",
+          icons: [],
+        },
+      });
+      console.log("client", web3wallet);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    if (!signClient) {
+      createClient();
+    }
+  }, [signClient]);
+  return <div></div>;
+};
+export default WalletConnect;
